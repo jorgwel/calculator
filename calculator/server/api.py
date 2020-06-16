79cc38815b9577a93bbc4e2ca63928d8b431bd9f
Jorge Bautista <jorgwel@gmail.com>
2020-06-16 13:51:33 -0500

    Refactoring static directory sctructure

--- calculator/server/api.py
+++ calculator/server/api.py
@@ -10,44 +10,44 @@ def serve_index():
 
 @app.route('/jquery-1.11.0.min.js')
 def serve_jquery():
-    return static_file('jquery-1.11.0.min.js', root='static/')
+    return static_file('jquery-1.11.0.min.js', root='static/externallibs/')
 
 
-@app.route('/script.js')
+@app.route('/main.js')
 def serve_jquery():
-    return static_file('script.js', root='static/')
+    return static_file('main.js', root='static/')
 
 
 @app.route('/big.min.js')
 def serve_jquery():
-    return static_file('big.min.js', root='static/')
+    return static_file('big.min.js', root='static/externallibs/')
 
 
 @app.route('/alu.js')
 def serve_jquery():
-    return static_file('alu.js', root='static/')
+    return static_file('alu.js', root='static/calculatorlibs/')
 
 
 @app.route('/operation.js')
 def serve_jquery():
-    return static_file('operation.js', root='static/')
+    return static_file('operation.js', root='static/calculatorlibs/')
 
 
 @app.route('/calculatorEngine.js')
 def serve_jquery():
-    return static_file('calculatorEngine.js', root='static/')
+    return static_file('calculatorEngine.js', root='static/calculatorlibs/')
 
 
 @app.route('/calculatorOuterBox.js')
 def serve_jquery():
-    return static_file('calculatorOuterBox.js', root='static/')
+    return static_file('calculatorOuterBox.js', root='static/calculatorlibs/')
 
 
 @app.route('/numberBuilder.js')
 def serve_jquery():
-    return static_file('numberBuilder.js', root='static/')
+    return static_file('numberBuilder.js', root='static/calculatorlibs/')
 
 
 @app.route('/calculatorBuilder.js')
 def serve_jquery():
-    return static_file('calculatorBuilder.js', root='static/')
+    return static_file('calculatorBuilder.js', root='static/calculatorlibs/')
